638d819fb0ce8e8b6487cabc1d981f9e4301b0ac
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-06-18 17:03:22 +0300

    Задача 6 готова

---
+++ HomeWork_003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int n;
+
+Console.Write("Input number ");
+n = Convert.ToInt32(Console.ReadLine ());
+
+if(n % 2 == 0)
+{
+    Console.WriteLine ("This number is even.");
+}
+else
+{
+    Console.WriteLine ("This number is not even.");
+}
