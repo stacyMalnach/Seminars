3079e822dfeb2f00a2e5aa4db77c19b1df3d9abb
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-06-21 13:11:20 +0300

    Испарвлено решение задачи 4

--- HomeWork_002/Program.cs
+++ HomeWork_002/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-int number1, number2, number3, sum;
+int number1, number2, number3, max;
 
 Console.Write("Input number1: ");
 number1 = Convert.ToInt32(Console.ReadLine ());
@@ -10,6 +10,29 @@ number2 = Convert.ToInt32(Console.ReadLine ());
 Console.Write("Input number3: ");
 number3 = Convert.ToInt32(Console.ReadLine ());
 
-sum = (number1 + number2 + number3);
-Console.Write($"Sum of all numbers are {sum}");
+if (number1 > number2)
+{
+    max = number1;
+}
+else 
+{
+    max = number2;
+}
 
+if (number1 > number3)
+{
+    max = number1;
+}
+else 
+{
+    max = number3;
+}
+if (number2 > number3)
+{
+    max = number1;
+}
+else 
+{
+    max = number3;
+}
+Console.Write($"Max number is {max}");
