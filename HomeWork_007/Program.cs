1ed0de4d705d15be5dd2073ecb0831880e97e93a
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-06-28 18:42:17 +0300

    Task 13

---
+++ HomeWork_007/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Numbers ()
+{
+int rand = new Random().Next(1,1000);
+Console.WriteLine ("Current number is " + rand );
+
+	if (rand < 100) 
+	{
+	Console.WriteLine("Third number is not exist"); 
+	return rand;
+	}
+	else
+	{
+	int current = rand % 10;
+	Console.WriteLine ("Third number is " + current);
+	return current;
+	}
+}
+
+int res = Numbers ();
+
