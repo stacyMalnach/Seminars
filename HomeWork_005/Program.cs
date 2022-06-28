7046382d2da5fbbbd19a0b1760b02a126891b67d
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-06-28 18:34:26 +0300

    Task 10

---
+++ HomeWork_005/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ 
+int Numbers (int n)
+{
+	int num = (n/10) % 10;
+	return num;
+}
+
+Console.WriteLine ("Input tree-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = Numbers (n);
+Console.WriteLine ("Second number is  " + res);
