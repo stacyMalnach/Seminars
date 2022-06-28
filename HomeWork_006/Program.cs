ddbd1ed665e37da6caf8dffc727e1d8912f5765f
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-06-28 18:25:21 +0300

    Task 15

---
+++ HomeWork_006/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Days(int n)
+{
+	if (n < 6) 
+		Console.WriteLine("This day is not day off =( ");
+	
+	else 
+		Console.WriteLine("This day is day off =) ");
+	
+}
+
+Console.WriteLine("Input number from 1 to 7");
+int n = Convert.ToInt32 (Console.ReadLine ());
+Days(n);
