c5e809c7866168637b98bd015a889a493968393a
Anastasia Pozniak <nastyapet050393@gmail.com>
2022-07-04 21:48:39 +0300

    Task 27

---
+++ HomeWork_011/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int FindSumDigit (int num)
+{
+    int sum = 0;
+    int digit = 0;
+
+    while (num > 0)
+    {
+        digit = num % 10;
+        sum += digit;
+        num = num / 10;
+    }
+
+    return sum; 
+}
+
+Console.Write ("Input random number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Digit's sum of your number is " + FindSumDigit (n));
